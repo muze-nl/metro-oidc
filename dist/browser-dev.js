dd1e0d7d6feba71aa43340d104162867796717fb
Auke van Slooten <auke@muze.nl>
2025-01-05 12:32:50 +0100

    updated dist, bumped version

--- dist/browser-dev.js
+++ dist/browser-dev.js
@@ -986,7 +986,7 @@
       // note: this allows path components in the options.issuer url
       configURL
     );
-    const openid_config = response2.body;
+    const openid_config = response2.body[Symbol.metroSource];
     assert(openid_config, openid_provider_metadata);
     assert(openid_config.issuer, options.issuer);
     return openid_config;
@@ -1500,6 +1500,26 @@
     }
   };
 
+  // src/oidc.store.mjs
+  function oidcStore(site) {
+    let store;
+    if (typeof localStorage !== "undefined") {
+      store = {
+        get: (name) => JSON.parse(localStorage.getItem("metro/oidc:" + site + ":" + name)),
+        set: (name, value) => localStorage.setItem("metro/oidc:" + site + ":" + name, JSON.stringify(value)),
+        has: (name) => localStorage.getItem("metro/oidc:" + site + ":" + name) !== null
+      };
+    } else {
+      let storeMap = /* @__PURE__ */ new Map();
+      store = {
+        get: (name) => JSON.parse(storeMap.get("metro/oidc:" + site + ":" + name) || null),
+        set: (name, value) => storeMap.set("metro/oidc:" + site + ":" + name, JSON.stringify(value)),
+        has: (name) => storeMap.has("metro/oidc:" + site + ":" + name)
+      };
+    }
+    return store;
+  }
+
   // src/oidcmw.mjs
   function oidcmw(options = {}) {
     const defaultOptions = {
@@ -1507,6 +1527,23 @@
       force_authorization: false
     };
     options = Object.assign({}, defaultOptions, options);
+    assert(options, {
+      client: Required(instanceOf(everything_default.client().constructor)),
+      // required because it is set in defaultOptions
+      client_info: Required(),
+      issuer: Required(validURL),
+      oauth2: Optional({}),
+      openid_configuration: Optional()
+    });
+    if (!options.store) {
+      options.store = oidcStore(options.issuer);
+    }
+    if (!options.openid_configuration && options.store.has("openid_configuration")) {
+      options.openid_configuration = options.store.get("openid_configuration");
+    }
+    if (!options.client_info.client_id && options.store.has("client_info")) {
+      options.client_info = options.store.get("client_info");
+    }
     return async (req, next) => {
       let res;
       if (!options.force_authorization) {
@@ -1525,6 +1562,7 @@
         options.openid_configuration = await oidcDiscovery({
           issuer: options.issuer
         });
+        options.store.set("openid_configuration", options.openid_configuration);
       }
       if (!options.client_info?.client_id) {
         assert(options.client_info?.client_name, Required());
@@ -1535,6 +1573,7 @@
           registration_endpoint: options.openid_configuration.registration_endpoint,
           client_info: options.client_info
         });
+        options.store.set("client_info", options.client_info);
       }
       const oauth2Options = Object.assign(
         {
@@ -1544,10 +1583,14 @@
           oauth2_configuration: {
             client_id: options.client_info.client_id,
             client_secret: options.client_info.client_secret,
+            code_verifier: false,
+            //disable pkce
             grant_type: "authorization_code",
             authorization_endpoint: options.openid_configuration.authorization_endpoint,
             token_endpoint: options.openid_configuration.token_endpoint,
-            scope: options.openid_configuration.scope || "openid"
+            scope: options.openid_configuration.scope || "openid",
+            redirect_uri: options.client_info.redirect_uris[0]
+            //FIXME: find the best match?
           }
         }
         //...
