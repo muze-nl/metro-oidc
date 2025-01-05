dc1bd05521fb6284350f2f395525ff3c7cbb0746
Auke van Slooten <auke@muze.nl>
2025-01-05 12:31:47 +0100

    unwrapped (de-proxied) openid_configuration result

--- src/oidc.discovery.mjs
+++ src/oidc.discovery.mjs
@@ -89,7 +89,7 @@ export default async function oidcDiscovery(options={}) {
 		// note: this allows path components in the options.issuer url
 		configURL
 	)
-	const openid_config = response.body
+	const openid_config = response.body[Symbol.metroSource]
 	assert(openid_config, openid_provider_metadata)
 
 	// https://openid.net/specs/openid-connect-discovery-1_0.html#ProviderConfigurationValidation
