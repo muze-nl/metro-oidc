f218bcb29c8b2397ef30a4d86101a8ae68ba4e19
Auke van Slooten <auke@muze.nl>
2025-01-07 13:43:22 +0100

    refactored to be inline with metro-oauth2

--- src/browser.mjs
+++ src/browser.mjs
@@ -2,10 +2,9 @@ import oidcDiscover from './oidc.discovery.mjs'
 import oidcRegister from './oidc.register.mjs'
 import oidcmw, {isRedirected} from './oidcmw.mjs'
 
-export const oidc = {
+export const oidc = Object.assign(oidcmw, {
 	discover: oidcDiscover,
 	register: oidcRegister,
-	oidcmw,
 	isRedirected
 }
 
