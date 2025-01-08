5321d617d7fd715b2765dbf1b9bd539d776b5507
Auke van Slooten <auke@muze.nl>
2025-01-08 11:46:20 +0100

    added idToken function

--- src/browser.mjs
+++ src/browser.mjs
@@ -1,11 +1,12 @@
 import oidcDiscover from './oidc.discovery.mjs'
 import oidcRegister from './oidc.register.mjs'
-import oidcmw, {isRedirected} from './oidcmw.mjs'
+import oidcmw, {isRedirected, idToken} from './oidcmw.mjs'
 
 export const oidc = Object.assign(oidcmw, {
 	discover: oidcDiscover,
 	register: oidcRegister,
-	isRedirected
+	isRedirected,
+	idToken
 })
 
 globalThis.oidc = oidc
