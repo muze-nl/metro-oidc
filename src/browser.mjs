325f8eb5f408ea4099f241d8ecc0e50f5ac2901b
Auke van Slooten <auke@muze.nl>
2025-01-14 10:18:42 +0100

    added metro and metro.oauth2 globals, added oidc object to globalThis.metro

--- src/browser.mjs
+++ src/browser.mjs
@@ -1,12 +1,19 @@
+import metro from '@muze-nl/metro'
+import oauth2 from '@muze-nl/metro-oauth2'
 import oidcDiscover from './oidc.discovery.mjs'
 import oidcRegister from './oidc.register.mjs'
 import oidcmw, {isRedirected, idToken} from './oidcmw.mjs'
 
-export const oidc = Object.assign(oidcmw, {
+const oidc = {
+	oidcmw,
 	discover: oidcDiscover,
 	register: oidcRegister,
 	isRedirected,
 	idToken
-})
+}
 
-globalThis.oidc = oidc
+if (!globalThis.metro.oidc) {
+	globalThis.metro.oidc = oidc
+}
+
+export default oidc
